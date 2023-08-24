ecf9305cb61e456e2850a656fbb88d5151eaff61
olkres <belirium29@gmail.com>
2023-08-24 22:21:55 +0300

    feat: made slider in section favorites

--- Library/index.js
+++ Library/index.js
@@ -1,3 +1,4 @@
+//burger
 (function () {
    const burgerItem = document.querySelector('.burger');
     const menu = document.querySelector('.nav');//обращается к menu и записывает его в переменную
@@ -9,12 +10,6 @@
         menu.classList.add('nav-active');
     });
 }());
-//const burger = document.querySelector('.burger');
-
-//function toggleMenu() {
-//  burger.classList.toggle('open');
-//}
-//burger.addEventListener('click', toggleMenu);
 
 // slider
 let offset = 0;
@@ -33,4 +28,52 @@ document.querySelector('.radio-2').addEventListener('click', function(){
 document.querySelector('.radio-3').addEventListener('click', function(){
     offset = - 950;
     sliderLine.style.left = offset + 'px';
+});
+
+//changing pictures
+const radioButtons = document.querySelectorAll('.favorites-radio');
+const items = document.querySelectorAll('.favorites__items');
+
+items.forEach(function(item) {
+  //Блок с active отображается
+  if (item.classList.contains('active')) {
+    item.style.display = 'flex';
+  } else {
+    item.style.display = 'none';
+  }
+});
+
+radioButtons.forEach(function(radio) {
+  radio.addEventListener('click', function() {
+    // Получаем значение атрибута data-season выбранной кнопки
+    const selectedSeason = this.parentElement.getAttribute('data-season');
+
+    // Перебираем все блоки .favorites__items
+    items.forEach(function(item) {
+      if (item.id === selectedSeason) {
+        // Если блок уже отображается, выходим из функции
+        if (item.style.display === 'flex') return;
+
+        // Удаляем active, если блок отображался ранее
+        if (item.classList.contains('active')) {
+          item.classList.remove('active');
+          item.style.opacity = 0;
+          setTimeout(function() {
+            item.style.display = 'none';
+          }, 1000);
+        } else {
+          // Применяем класс для fade in, если блок не отображался ранее
+          item.style.display = 'flex';
+          setTimeout(function() {
+            item.style.opacity = 1;
+          }, 50);
+        }
+      } else {
+        // Прячем остальные блоки
+        item.style.display = 'none';
+        item.classList.remove('active');
+        item.style.opacity = 0;
+      }
+    });
+  });
 });
