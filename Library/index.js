598a5a160330166d256b3294eac238ce40499654
olkres <belirium29@gmail.com>
2023-09-19 20:14:20 +0300

    feat: local storage js

--- Library/index.js
+++ Library/index.js
@@ -150,29 +150,34 @@ openDropMenuRegister.addEventListener('click', () =>{
 
 //LOCAL STORAGE
 
+// const storageUsers = localStorage.getItem("users");
+// let allUsers = storedUsers ? JSON.parse(storageUsers) : [];
+// localStorage.setItem("users", JSON.stringify(allUsers));
+const menuProfileIcon = document.querySelector('.profile');
+const dropMenuAutorization = document.querySelector('.drop-menu');
+
+let userInfo;
+
+let formData = {};
+
 const registrationForm = document.querySelector('.modal-register__form');
-const firstNameInput = document.getElementById('first__name');
-const lastNameInput = document.getElementById('last__name');
-const emailInput = document.getElementById('register__email');
-const passwordInput = document.getElementById('register__password');
+// const firstNameInput = document.getElementById('first__name');
+// const lastNameInput = document.getElementById('last__name');
+// const emailInput = document.getElementById('register__email');
+// const passwordInput = document.getElementById('register__password');
 // const registerButton = document.querySelector('.modal-register__button');
 
-function generateRandomCardNumber() {
-  const min = 0x10000000;
-  const max = 0xffffffff;
-  const randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
-  const cardNumber = randomNumber.toString(16).toUpperCase().padStart(9, '0');
-  return cardNumber;
-}
+
 
 // отправка формы
 registrationForm.addEventListener('submit', function(event) {
   event.preventDefault();
 
-  const firstName = firstNameInput.value;
-  const lastName = lastNameInput.value;
-  const email = emailInput.value;
-  const password = passwordInput.value;
+  const firstName = document.getElementById('first__name').value;
+  const lastName = document.getElementById('last__name').value;
+  const email = document.getElementById('register__email').value;
+  const password = document.getElementById('register__password').value;
+  const cardNumber = (Math.floor(Math.random() * (0xffffffff - 0x10000000 + 1)) + 0x10000000).toString(16).toUpperCase().padStart(9, '0');
 
   // длина пароля
   if (password.length < 8) {
@@ -210,44 +215,117 @@ registrationForm.addEventListener('submit', function(event) {
     lastName: lastName,
     email: email,
     password: password,
-    сardNumber: generateRandomCardNumber()
+    сardNumber: cardNumber,
+    listBooks: [],
+    booksCounter: 0,
+    counterAutorization: 0,
+    abonement: false,
+    autorization: false,
+    registration: false
   };
 
+  // const user = new userData(firstName, lastName, email, password, cardNumber);
+
   // добавление нового пользователя в массив
-  users.push(userData);
+  // users.push(userData);
 
   // сохранение обновленного массива
   localStorage.setItem('users', JSON.stringify(users));
-  
-  // очистка полей формы
-  firstNameInput.value = '';
-  lastNameInput.value = '';
-  emailInput.value = '';
-  passwordInput.value = '';
+
+  let checkReg = false;
+
+  users.forEach(item => {
+
+    if(item.email === userData.email){
+       checkReg = true;
+    }else{
+       checkReg = false;
+    }
+
+ })
+
+ if(checkReg){
+    alert('такой почтовый ящик уже зарегестрирован');
+ }else{
+
+    userInfo = userData;
+    // userData.registration = true;
+    users.push(userData)
+    localStorage.setItem('users', JSON.stringify(users));
+
+    updateAutorization()
+
+    updateUsers()
+
+    // clearForms()
+
+ }
 });
 
-//АВТОРИЗАЦИЯ
-const loginForm = document.querySelector('.modal-login__form');
-const loginEmailInput = document.getElementById('login__email');
-const loginPasswordInput = document.getElementById('login__password');
+function updateAutorization(){
+  checkActiveUser = true;
 
-loginForm.addEventListener('submit', function(event) {
-  event.preventDefault();
+  users = JSON.parse(localStorage.getItem('users'));
 
-  const enteredEmail = loginEmailInput.value;
-  const enteredPassword = loginPasswordInput.value;
-
-  // получение существующих пользователей
-  const users = JSON.parse(localStorage.getItem('users')) || [];
-
-  // поиск пользователя с введенным email и паролем
-  const loggedInUser = users.find(user => user.email === enteredEmail && user.password === enteredPassword);
-
-  if (loggedInUser) {
-    location.reload();
-  } else {
-    alert('Неверные данные для авторизации');
+  for (let i = 0; i < users.length; i++) {
+     if (users[i].email === userInfo.email) {
+        users[i].autorization = true;
+        users[i].counterAutorization += 1;
+        userInfo = users[i];
+        break;
+     }
   }
-});
+
+  localStorage.setItem('users', JSON.stringify(users));
+};
+
+function updateUsers() {
+  users = JSON.parse(localStorage.getItem('users'));
+      
+  for (let i = 0; i < users.length; i++) {
+     if (users[i].email === userInfo.email) {
+        userInfo = users[i];
+        break;
+     }
+  }
+  
+  localStorage.setItem('users', JSON.stringify(users));
+
+  menuProfileIcon.innerHTML = '<span class="log-in-btn__title" title=${userInfo.firstName}${userInfo.lastName}>${userInfo.name[0] + userInfo.surname[0]}</span>'
+
+  dropMenuAutorization.innerHTML = `<div class="drop-menu">
+      <div class="drop-menu__profile-id">${userInfo.cardNumber}</div>
+      <div class="drop-menu__login">My profile</div>
+      <div class="drop-menu__register">Log Out</div>
+      </div>`
+}
+
+function defaultView() {
+  checkActiveUser = false;
+      
+  users = JSON.parse(localStorage.getItem('users'));
+      
+      for (let i = 0; i < users.length; i++) {
+         if (users[i].email === userInfo.email) {
+            users[i].autorization = false;
+            userInfo = users[i];
+            break;
+         }
+      }
+      
+      localStorage.setItem('users', JSON.stringify(users));
+
+      //стандартная иконка пользователя
+      menuProfileIcon.innerHTML = `<svg width="28" height="28" viewBox="0 0 28 28" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path fill-rule="evenodd" clip-rule="evenodd" d="M28 14C28 21.732 21.732 28 14 28C6.26801 28 0 21.732 0 14C0 6.26801 6.26801 0 14 0C21.732 0 28 6.26801 28 14ZM18.6667 7.77778C18.6667 10.3551 16.5774 12.4444 14.0001 12.4444C11.4227 12.4444 9.33339 10.3551 9.33339 7.77778C9.33339 5.20045 11.4227 3.11111 14.0001 3.11111C16.5774 3.11111 18.6667 5.20045 18.6667 7.77778ZM19.4998 16.2781C20.9584 17.7367 21.7778 19.715 21.7778 21.7778H14L6.22225 21.7778C6.22225 19.715 7.0417 17.7367 8.50031 16.2781C9.95893 14.8194 11.9372 14 14 14C16.0628 14 18.0411 14.8194 19.4998 16.2781Z" fill="white"/>
+      </svg>`
+
+      //drop menu
+      dropMenuAutorization.innerHTML = `<div class="drop-menu">
+      <div class="drop-menu__profile">Profile</div>
+      <div class="drop-menu__login">Log In</div>
+      <div class="drop-menu__register">Register</div>
+      </div>`
+};
 
 
